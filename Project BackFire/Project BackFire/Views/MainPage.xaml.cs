2567c0f2f6957f01ba11a68fc891c43fcdd91e52
lubbejareb <ludwigjareb@gmail.com>
2018-04-16 11:23:08 +0200

    menu

--- Project BackFire/Project BackFire/Views/MainPage.xaml.cs
+++ Project BackFire/Project BackFire/Views/MainPage.xaml.cs
@@ -2,6 +2,10 @@
 using Project_BackFire.ViewModels;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Input;
+using Windows.UI.ViewManagement;
 
 namespace Project_BackFire.Views
 {
@@ -14,31 +18,20 @@ namespace Project_BackFire.Views
 
         public MainPage()
         {
-            InitializeComponent();
+
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
+            InitializeComponent();              
         }
 
 
         private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
         {
             string HourMinute;
-
-            HourMinute = DateTime.Now.ToString("HH:mm");
-        }
-
-        private void button1_PointerEntered(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
-        {
-            button1.Visibility = Visibility.Visible;
-        }
-
-        private void button1_PointerExited(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
-        {
-<<<<<<< HEAD
-            button1.Visibility = Visibility.Collapsed;
-=======
             DateTime TimeNow = new DateTime();
             TimeNow = DateTime.Now;
             String CurrentDatetime = TimeNow.ToString();
->>>>>>> c11d9d85dd18f9f1675a84752ebd15fb1e23e18f
+            HourMinute = DateTime.Now.ToString("HH:mm");
         }
+
     }
 }
