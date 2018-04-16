2d26db772ccf04d99adada5ac26e6f086639c74d
lubbejareb <ludwigjareb@gmail.com>
2018-04-16 11:27:43 +0200

    menu

--- Project BackFire/Project BackFire/Views/MainPage.xaml.cs
+++ Project BackFire/Project BackFire/Views/MainPage.xaml.cs
@@ -30,31 +30,11 @@ namespace Project_BackFire.Views
             string HourMinute;
             DateTime TimeNow = new DateTime();
             TimeNow = DateTime.Now;
-<<<<<<< HEAD
+
             string CurrentDatetime = TimeNow.ToString();
         }
 
-        private void AddTime()
-        {
-            try
-            {
-                TextBlock TimeDate = MainPage.Current.FindName("TimeDate") as TextBlock;
-                string sLog = TimeDate.Text;
-                string sNew;
-            }
-            catch
-            {
-
-            }
-=======
-            String CurrentDatetime = TimeNow.ToString();
-<<<<<<< HEAD
-            HourMinute = DateTime.Now.ToString("HH:mm");
-=======
->>>>>>> c11d9d85dd18f9f1675a84752ebd15fb1e23e18f
->>>>>>> b1dc87d44d204479753a732f52fa4f9f25f19461
->>>>>>> 11b9eb8390a9a9da48b92b0d04061ba98ad053b6
-        }
+     
 
     }
 }
