faa42639f2bafded94df5edae82f9f88c86f3267
Ludwig <luddw.pantzar@outlook.com>
2018-04-17 11:27:02 +0200

    Addded animation som är helt i onödan hej

--- Project BackFire/Project BackFire/Views/MainPage.xaml.cs
+++ Project BackFire/Project BackFire/Views/MainPage.xaml.cs
@@ -9,12 +9,15 @@ using Windows.UI.ViewManagement;
 using System.Globalization;
 using System.Threading;
 using Windows.UI;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.Graphics.Effects;
 
 namespace Project_BackFire.Views
 {
     public sealed partial class MainPage : Page
     {
         DispatcherTimer Timer = new DispatcherTimer();
+        private Storyboard rotation = new Storyboard();
 
         private MainViewModel ViewModel
         {
@@ -29,13 +32,58 @@ namespace Project_BackFire.Views
             Timer.Interval = new TimeSpan(0, 0, 1);
             Timer.Start();
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
+
             InitializeComponent();
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0.0;
+            animation.To = 360.0;
+            animation.BeginTime = TimeSpan.FromSeconds(1);
+            animation.RepeatBehavior = RepeatBehavior.Forever;
+            Storyboard.SetTarget(animation, Img1);
+            Storyboard.SetTargetProperty(animation, "(UIElement.Projection).(PlaneProjecton.RotationY)");
+            rotation.Children.Clear();
+            rotation.Children.Add(animation);
+            switch (animation.To)
+            {
+                case 180.0:
+                    {
+                        rotation.Stop();
+                        break;
+                    }
+
+                case 360.0:
+                    {
+                        rotation.Stop();
+                        break;
+                    }
+
+                default:
+                    {
+                        rotation.Begin();
+                        break;
+                    }
+            }
         }
 
         private void TimerTick(object sender, object e)
         {
             TimeDate.Text = DateTime.Now.ToString("HH:mm");
             TodaysDate.Text = DateTime.Today.ToString("dd/MM/yyyy");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            rotation.Begin();
+        }
+
+        private void Img1_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            rotation.Begin();
+        }
+
+        private void Img1_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            rotation.Stop();
         }
 
         private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
@@ -96,22 +144,17 @@ namespace Project_BackFire.Views
             switch (value)
             {
                 case 1:
-                    grid1.Background = (SolidColorBrush)Resources["red"];
+                    Grid1.Background = (SolidColorBrush)Resources["red"];
                     break;
                 case 2:
-                    grid1.Background = (SolidColorBrush)Resources["yellow"];
+                    Grid1.Background = (SolidColorBrush)Resources["yellow"];
                     break;
 
                 default:
-                    grid1.Background = (SolidColorBrush)Resources["green"];
+                    Grid1.Background = (SolidColorBrush)Resources["green"];
                     break;
             }
 
-        }
-
-        private void Grid1_Loaded(object sender, RoutedEventArgs e)
-        {
-            
         }
     }
 }
