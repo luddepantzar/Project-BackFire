4bf5a7f6d25f066e0c4780d019d9821195db78fc
Ludwig <luddw.pantzar@outlook.com>
2018-04-20 10:47:54 +0200

    Added button effects

--- Project BackFire/Project BackFire/Views/MainPage.xaml.cs
+++ Project BackFire/Project BackFire/Views/MainPage.xaml.cs
@@ -40,6 +40,14 @@ namespace Project_BackFire.Views
         private Storyboard rotationfront6 = new Storyboard();
         private Storyboard rotationfront7 = new Storyboard();
         private Storyboard rotationfront8 = new Storyboard();
+        private Storyboard rotationBorder1 = new Storyboard();
+        private Storyboard rotationBorder2= new Storyboard();
+        private Storyboard rotationBorder3 = new Storyboard();
+        private Storyboard rotationBorder4 = new Storyboard();
+        private Storyboard rotationBorder5 = new Storyboard();
+        private Storyboard rotationBorder6 = new Storyboard();
+        private Storyboard rotationBorder7 = new Storyboard();
+        private Storyboard rotationBorder8 = new Storyboard();
 
         private MainViewModel ViewModel
         {
@@ -54,17 +62,11 @@ namespace Project_BackFire.Views
             Timer.Interval = new TimeSpan(0, 0, 1);
             Timer.Start();
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
-<<<<<<< HEAD
             AnimationBack();
             AnimationFront();
             FlipCardConditions();
-=======
-            //AnimationBack();
-            //AnimationFront();
-
->>>>>>> b8a3bff296553d9a1407b2c60dede14eb9df4162
         }
-/*
+
         public void AnimationFront()
         {
             List = new List<DependencyObject>();
@@ -236,7 +238,7 @@ namespace Project_BackFire.Views
             animation12.BeginTime = TimeSpan.FromSeconds(0);
             animation12.RepeatBehavior = new RepeatBehavior(1);
             animation12.Duration = TimeSpan.FromSeconds(3);
-            Storyboard.SetTarget(animation12, list[0]);
+            Storyboard.SetTarget(animation12, list[5]);
             Storyboard.SetTargetProperty(animation12, "(UIElement.Projection).(PlaneProjection.RotationY)");
             rotationback6.Children.Clear();
             rotationback6.Children.Add(animation12);
@@ -247,7 +249,7 @@ namespace Project_BackFire.Views
             animation14.BeginTime = TimeSpan.FromSeconds(0);
             animation14.RepeatBehavior = new RepeatBehavior(1);
             animation14.Duration = TimeSpan.FromSeconds(3);
-            Storyboard.SetTarget(animation14, list[0]);
+            Storyboard.SetTarget(animation14, list[6]);
             Storyboard.SetTargetProperty(animation14, "(UIElement.Projection).(PlaneProjection.RotationY)");
             rotationback7.Children.Clear();
             rotationback7.Children.Add(animation14);
@@ -258,19 +260,110 @@ namespace Project_BackFire.Views
             animation16.BeginTime = TimeSpan.FromSeconds(0);
             animation16.RepeatBehavior = new RepeatBehavior(1);
             animation16.Duration = TimeSpan.FromSeconds(3);
-            Storyboard.SetTarget(animation16, list[0]);
+            Storyboard.SetTarget(animation16, list[7]);
             Storyboard.SetTargetProperty(animation16, "(UIElement.Projection).(PlaneProjection.RotationY)");
             rotationback8.Children.Clear();
             rotationback8.Children.Add(animation16);
         }
-        */
+
+        public void BorderAnimation()
+        {
+            DependencyObject[] BorderList = new DependencyObject[8] { grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8 };
+            DoubleAnimation animation17 = new DoubleAnimation();
+            animation17.From = 180;
+            animation17.To = 360;
+            animation17.BeginTime = TimeSpan.FromSeconds(0);
+            animation17.RepeatBehavior = new RepeatBehavior(1);
+            animation17.Duration = TimeSpan.FromSeconds(3);
+            Storyboard.SetTarget(animation17, BorderList[0]);
+            Storyboard.SetTargetProperty(animation17, "(UIElement.Projection).(PlaneProjection.RotationY)");
+            rotationBorder1.Children.Clear();
+            rotationBorder1.Children.Add(animation17);
+
+            DoubleAnimation animation18 = new DoubleAnimation();
+            animation18.From = 180;
+            animation18.To = 360;
+            animation18.BeginTime = TimeSpan.FromSeconds(0);
+            animation18.RepeatBehavior = new RepeatBehavior(1);
+            animation18.Duration = TimeSpan.FromSeconds(3);
+            Storyboard.SetTarget(animation18, BorderList[1]);
+            Storyboard.SetTargetProperty(animation18, "(UIElement.Projection).(PlaneProjection.RotationY)");
+            rotationBorder2.Children.Clear();
+            rotationBorder2.Children.Add(animation18);
+
+            DoubleAnimation animation19 = new DoubleAnimation();
+            animation19.From = 180;
+            animation19.To = 360;
+            animation19.BeginTime = TimeSpan.FromSeconds(0);
+            animation19.RepeatBehavior = new RepeatBehavior(1);
+            animation19.Duration = TimeSpan.FromSeconds(3);
+            Storyboard.SetTarget(animation19, BorderList[2]);
+            Storyboard.SetTargetProperty(animation19, "(UIElement.Projection).(PlaneProjection.RotationY)");
+            rotationBorder3.Children.Clear();
+            rotationBorder3.Children.Add(animation19);
+
+            DoubleAnimation animation20 = new DoubleAnimation();
+            animation20.From = 180;
+            animation20.To = 360;
+            animation20.BeginTime = TimeSpan.FromSeconds(0);
+            animation20.RepeatBehavior = new RepeatBehavior(1);
+            animation20.Duration = TimeSpan.FromSeconds(3);
+            Storyboard.SetTarget(animation20, BorderList[3]);
+            Storyboard.SetTargetProperty(animation20, "(UIElement.Projection).(PlaneProjection.RotationY)");
+            rotationBorder4.Children.Clear();
+            rotationBorder4.Children.Add(animation20);
+
+            DoubleAnimation animation21 = new DoubleAnimation();
+            animation21.From = 180;
+            animation21.To = 360;
+            animation21.BeginTime = TimeSpan.FromSeconds(0);
+            animation21.RepeatBehavior = new RepeatBehavior(1);
+            animation21.Duration = TimeSpan.FromSeconds(3);
+            Storyboard.SetTarget(animation21, BorderList[4]);
+            Storyboard.SetTargetProperty(animation21, "(UIElement.Projection).(PlaneProjection.RotationY)");
+            rotationBorder5.Children.Clear();
+            rotationBorder5.Children.Add(animation21);
+
+            DoubleAnimation animation22 = new DoubleAnimation();
+            animation22.From = 180;
+            animation22.To = 360;
+            animation22.BeginTime = TimeSpan.FromSeconds(0);
+            animation22.RepeatBehavior = new RepeatBehavior(1);
+            animation22.Duration = TimeSpan.FromSeconds(3);
+            Storyboard.SetTarget(animation22, BorderList[5]);
+            Storyboard.SetTargetProperty(animation22, "(UIElement.Projection).(PlaneProjection.RotationY)");
+            rotationBorder6.Children.Clear();
+            rotationBorder6.Children.Add(animation22);
+
+            DoubleAnimation animation23 = new DoubleAnimation();
+            animation23.From = 180;
+            animation23.To = 360;
+            animation23.BeginTime = TimeSpan.FromSeconds(0);
+            animation23.RepeatBehavior = new RepeatBehavior(1);
+            animation23.Duration = TimeSpan.FromSeconds(3);
+            Storyboard.SetTarget(animation23, BorderList[6]);
+            Storyboard.SetTargetProperty(animation23, "(UIElement.Projection).(PlaneProjection.RotationY)");
+            rotationBorder7.Children.Clear();
+            rotationBorder7.Children.Add(animation23);
+
+            DoubleAnimation animation24 = new DoubleAnimation();
+            animation24.From = 180;
+            animation24.To = 360;
+            animation24.BeginTime = TimeSpan.FromSeconds(0);
+            animation24.RepeatBehavior = new RepeatBehavior(1);
+            animation24.Duration = TimeSpan.FromSeconds(3);
+            Storyboard.SetTarget(animation24, BorderList[7]);
+            Storyboard.SetTargetProperty(animation24, "(UIElement.Projection).(PlaneProjection.RotationY)");
+            rotationBorder8.Children.Clear();
+            rotationBorder8.Children.Add(animation24);
+        }
         
         private void TimerTick(object sender, object e)
         {
             TimeDate.Text = DateTime.Now.ToString("HH:mm");
             TodaysDate.Text = DateTime.Today.ToString("dd/MM/yyyy");
         }
-        /*
+ 
         public void OpacityImage2()
         {
             BackImg1.Opacity = 0;
@@ -373,12 +466,20 @@ namespace Project_BackFire.Views
             rotationback6.Begin();
             rotationback7.Begin();
             rotationback8.Begin();
+            rotationBorder1.Begin();
+            rotationBorder2.Begin();
+            rotationBorder3.Begin();
+            rotationBorder4.Begin();
+            rotationBorder5.Begin();
+            rotationBorder6.Begin();
+            rotationBorder7.Begin();
+            rotationBorder8.Begin();
             OpacityImage2();
         }
-        */
+
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
-            //Animation();
+            Animation();
         }
 
         private void FlipCardConditions()
@@ -387,6 +488,7 @@ namespace Project_BackFire.Views
             {
                 rotationback1.Stop();
                 rotationfront1.Stop();
+                //rotationBorder1.Stop();
             }
             else if (List.Count > 8)
             {
@@ -398,6 +500,7 @@ namespace Project_BackFire.Views
                 {
                     rotationback1.Begin();
                     rotationfront1.Begin();
+                    //rotationBorder1.Begin();
                     tm8.Stop();
                 };
                 tm8.Start();
@@ -436,11 +539,13 @@ namespace Project_BackFire.Views
             MenuBar.Background = (SolidColorBrush)Resources["MenubarColor1"];
 
         }
+
         private void color2_Click(object sender, RoutedEventArgs e)
         {
             ContentArea.Background = (SolidColorBrush)Resources["BackgroundColor2"];
             MenuBar.Background = (SolidColorBrush)Resources["MenubarColor2"];
         }
+
         private void color3_Click(object sender, RoutedEventArgs e)
         {
             ContentArea.Background = (SolidColorBrush)Resources["BackgroundColor3"];
@@ -470,5 +575,26 @@ namespace Project_BackFire.Views
             }
             */
         }
+
+        public void OnPointerMoved(object Send ,PointerRoutedEventArgs e)
+        {
+            SettinsButton.Visibility = Visibility.Visible;
+            DispatcherTimer ButtonTimer = new DispatcherTimer();
+            ButtonTimer.Interval = TimeSpan.FromSeconds(5);
+            ButtonTimer.Tick += (sender, args) =>
+            {
+                SettinsButton.Visibility = Visibility.Collapsed;
+                ButtonTimer.Stop();
+            };
+            ButtonTimer.Start();
+        }
     }
 }
+
+
+
+
+
+
+
+
