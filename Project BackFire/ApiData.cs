ec1b43b5a639ab78e1219a1ed888af2277cfbfac
lubbejareb <ludwigjareb@gmail.com>
2018-05-08 13:16:09 +0200

    apidata

---
+++ Project BackFire/ApiData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_BackFire
+{
+    class ApiData
+    {
+    }
+}
+
+public class Rootobject
+{
+    public Class1[] Property1 { get; set; }
+
+
+    public string name { get; set; }
+    public string id { get; set; }
+    public DateTime createdDate { get; set; }
+    public string code { get; set; }
+    public string mainColor { get; set; }
+    public int timeInterval { get; set; }
+    public int maxSeats { get; set; }
+}
+
+public class Class1
+{
+   
+}
