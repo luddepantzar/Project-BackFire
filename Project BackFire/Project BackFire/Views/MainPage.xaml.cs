2e2c92ec7c61bfa576b5681190fed44af1ab6a8c
lubbejareb <ludwigjareb@gmail.com>
2018-05-15 15:49:41 +0200

    Removeed code

--- Project BackFire/Project BackFire/Views/MainPage.xaml.cs
+++ Project BackFire/Project BackFire/Views/MainPage.xaml.cs
@@ -26,17 +26,15 @@ namespace Project_BackFire.Views
 
         public ObservableCollection<Attributes> attributes;
 
-        public bool projactor { get; set; }
 
-        public int Count { get; set; }
-        private Storyboard rotationfront1 = new Storyboard();
-        private Storyboard rotationback1 = new Storyboard();
+        //public int Count { get; set; }
+
+        //private Storyboard rotationfront1 = new Storyboard();
+        //private Storyboard rotationback1 = new Storyboard();
+
         public DispatcherTimer DisTimer = new DispatcherTimer();
-        private TimeSpan SpanTime;
 
-        //public static BitmapImage One = new BitmapImage(new Uri("ms-appx///Images/c1.jpeg"));
-        //public static BitmapImage Two = new BitmapImage(new Uri("ms-appx///Images/c2.jpeg"));
-        //public static BitmapImage Three = new BitmapImage(new Uri("ms-appx///Images/c3.jpg"));
+        private TimeSpan SpanTime;
 
         private MainViewModel ViewModel
         {
@@ -54,76 +52,18 @@ namespace Project_BackFire.Views
             //AnimationBack();
             //AnimationFront();                     
             //FlipCardConditions();
-            hidesettinggrid();
-<<<<<<< HEAD
-            //Attributes = AttributeManager.GetAttributes();
-        }
-
-        public void SwitchAttributes()
-        {
-            Attributes c1 = new Attributes { RoomID = 10, RoomName = "Rum 10", FrontImage = "ms-appx:///Images/c1.jpeg", BackImage = "ms-appx:///Images/c2.jpeg",
-                                                Projector = true, Wboard = false, Tv = false, Wifi = true, Seats = 5, Note = "Lorem ipsum dolor sit amet, co" };
-
-            switch (c1.Projector)
-            {
-                case false:
-                    {
-                        CheckProj.Visibility = Visibility.Collapsed;
-                        ErrProj.Visibility = Visibility.Visible;
-                        break;
-                    }
-
-                case true:
-                    {
-                        ErrProj.Visibility = Visibility.Collapsed;
-                        CheckProj.Visibility = Visibility.Visible;
-                        break;
-                    }
-            }
-
-            switch (c1.Wboard)
-            {
-                case false:
-                    {
-                        CheckWhiteboard.Visibility = Visibility.Collapsed;
-                        ErrWboard.Visibility = Visibility.Visible;
-                        break;
-                    }
-=======
->>>>>>> a4ca9df9fa646614d4db73ba51efeb62978b1f22
 
             attributes = new ObservableCollection<Attributes>();
 
             attributes.Add(new Attributes { RoomID = 1, RoomName = "Rum 1", Projector = true, Wboard = false, Tv = false, Wifi = true, Seats = 5, Note = "Lorem ipsum dolor sit amet, co" });
-            attributes.Add(new Attributes { RoomID = 2, RoomName = "Rum 2", Projector = false, Wboard = true, Tv = true, Wifi = true, Seats = 10  });
+            attributes.Add(new Attributes { RoomID = 2, RoomName = "Rum 2", Projector = false, Wboard = true, Tv = true, Wifi = true, Seats = 10 });
             attributes.Add(new Attributes { RoomID = 3, RoomName = "Rum 3", Projector = false, Wboard = false, Tv = true, Wifi = false, Seats = 4 });
             attributes.Add(new Attributes { RoomID = 4, RoomName = "Rum 4", Projector = true, Wboard = true, Tv = false, Wifi = true, Seats = 15 });
             attributes.Add(new Attributes { RoomID = 5, RoomName = "Rum 5", Projector = true, Wboard = false, Tv = false, Wifi = true, Seats = 7 });
             attributes.Add(new Attributes { RoomID = 6, RoomName = "Rum 6", Projector = false, Wboard = true, Tv = true, Wifi = false, Seats = 22 });
             attributes.Add(new Attributes { RoomID = 7, RoomName = "Rum 7", Projector = false, Wboard = false, Tv = true, Wifi = true, Seats = 12 });
             attributes.Add(new Attributes { RoomID = 8, RoomName = "Rum 8", Projector = true, Wboard = true, Tv = false, Wifi = false, Seats = 30 });
-
-<<<<<<< HEAD
-            switch (c1.Wifi)
-            {
-                case false:
-                    {
-                        CheckWifi.Visibility = Visibility.Collapsed;
-                        ErrWifi.Visibility = Visibility.Visible;
-                        break;
-                    }
-
-                case true:
-                    {
-                        CheckWifi.Visibility = Visibility.Visible;
-                        ErrWifi.Visibility = Visibility.Collapsed;
-                        break;
-                    }
-            }
-=======
->>>>>>> a4ca9df9fa646614d4db73ba51efeb62978b1f22
         }
-
 
         async void Getname()
         {
@@ -433,64 +373,64 @@ namespace Project_BackFire.Views
 
         private void FlipCardConditions()
         {
-            //if (Attributes.Count < 8)
-            //{
-            //    rotationback1.Stop();
-            //    rotationfront1.Stop();
-            //}
+            //    //if (Attributes.Count < 8)
+            //    //{
+            //    //    rotationback1.Stop();
+            //    //    rotationfront1.Stop();
+            //    //}
 
-            //while (Attributes.Count > 8)
-            //{
+            //    //while (Attributes.Count > 8)
+            //    //{
 
-            //    try
-            //    {
-            //        rotationback1.Stop();
-            //        rotationfront1.Stop();
-            //        DispatcherTimer tm8 = new DispatcherTimer();
-            //        tm8.Interval = TimeSpan.FromSeconds(10);
-            //        tm8.Tick += (sender, args) =>
-            //        {
-            //            rotationback1.Begin();
-            //            rotationfront1.Begin();
+            //    //    try
+            //    //    {
+            //    //        rotationback1.Stop();
+            //    //        rotationfront1.Stop();
+            //    //        DispatcherTimer tm8 = new DispatcherTimer();
+            //    //        tm8.Interval = TimeSpan.FromSeconds(10);
+            //    //        tm8.Tick += (sender, args) =>
+            //    //        {
+            //    //            rotationback1.Begin();
+            //    //            rotationfront1.Begin();
 
-            //            BackImg1.Opacity = 0;
+            //    //            BackImg1.Opacity = 0;
 
-            //            grid1.Opacity = 0;
+            //    //            grid1.Opacity = 0;
 
-            //            Imgbox1.Fade(duration: 1000, delay: 0, value: 0).Start();
+            //    //            Imgbox1.Fade(duration: 1000, delay: 0, value: 0).Start();
 
-            //            grid1.Fade(duration: 4000, delay: 3000, value: 1).Start();
+            //    //            grid1.Fade(duration: 4000, delay: 3000, value: 1).Start();
 
-            //            BackImg1.Fade(duration: 4000, delay: 1200, value: 1).Start();
+            //    //            BackImg1.Fade(duration: 4000, delay: 1200, value: 1).Start();
 
-            //            tm8.Stop();
-            //        };
-            //        tm8.Start();
-            //    }
-            //    finally
-            //    {
-            //        rotationback1.Stop();
-            //        rotationfront1.Stop();
-            //    }
-            //}
+            //    //            tm8.Stop();
+            //    //        };
+            //    //        tm8.Start();
+            //    //    }
+            //    //    finally
+            //    //    {
+            //    //        rotationback1.Stop();
+            //    //        rotationfront1.Stop();
+            //    //    }
+            //    //}
         }
 
         public void StartTimer(int IntervalSec)
         {
-            //CmdBar.ClosedDisplayMode = AppBarClosedDisplayMode.Compact;
-            //if (IntervalSec > 0)
-            //{
-            //    DisTimer.Interval = new TimeSpan(0, 0, IntervalSec);
-            //    DisTimer.Tick += TimerOnTick;
-            //    CmdBar.ClosedDisplayMode = AppBarClosedDisplayMode.Hidden;
-            //    DisTimer.Start();
-            //}
-            //else
-            //{
-            //    CmdBar.ClosedDisplayMode = AppBarClosedDisplayMode.Hidden;
-            //    DisTimer.Stop();
-            //    _allowexicution = false;
-            //}
+            //    //CmdBar.ClosedDisplayMode = AppBarClosedDisplayMode.Compact;
+            //    //if (IntervalSec > 0)
+            //    //{
+            //    //    DisTimer.Interval = new TimeSpan(0, 0, IntervalSec);
+            //    //    DisTimer.Tick += TimerOnTick;
+            //    //    CmdBar.ClosedDisplayMode = AppBarClosedDisplayMode.Hidden;
+            //    //    DisTimer.Start();
+            //    //}
+            //    //else
+            //    //{
+            //    //    CmdBar.ClosedDisplayMode = AppBarClosedDisplayMode.Hidden;
+            //    //    DisTimer.Stop();
+            //    //    _allowexicution = false;
+            //    //}
         }
 
         public void TimerOnTick(object sender, object o)
@@ -540,15 +480,6 @@ namespace Project_BackFire.Views
             ApiSettingsGrid.Visibility = Visibility.Collapsed;
         }
 
-        public void hidesettinggrid()
-        {
-            switch (SettingsGrid)
-            {
-
-            }
-
-        }
-
         private void ContentArea_Tapped(object Sender, TappedRoutedEventArgs e)
         {
             CmdBar.ClosedDisplayMode = AppBarClosedDisplayMode.Compact;
@@ -564,156 +495,45 @@ namespace Project_BackFire.Views
 
         private void btn2_Click(object sender, RoutedEventArgs e)
         {
-<<<<<<< HEAD
-            LinearGradientBrush RedBrush = (LinearGradientBrush)Resources["RedLinearBrush"];
+            //    LinearGradientBrush RedBrush = (LinearGradientBrush)Resources["RedLinearBrush"];
 
-            RectangelGrid2.Fill = RedBrush;
+            //    RectangelGrid2.Fill = RedBrush;
 
-            DispatcherTimer ColorTimer = new DispatcherTimer();
-            ColorTimer.Interval = TimeSpan.FromSeconds(5);
-            ColorTimer.Tick += (Sender, args) =>
-            {
-                YellowindIcatorColorToGreenIndicatorColor();
-                ColorTimer.Stop();
-            };
-            ColorTimer.Start();
-=======
+            //    DispatcherTimer ColorTimer = new DispatcherTimer();
+            //    ColorTimer.Interval = TimeSpan.FromSeconds(5);
+            //    ColorTimer.Tick += (Sender, args) =>
+            //    {
+            //        YellowindIcatorColorToGreenIndicatorColor();
+            //        ColorTimer.Stop();
+            //    };
+            //    ColorTimer.Start();
 
-<<<<<<< HEAD
 
-=======
-            
->>>>>>> a4ca9df9fa646614d4db73ba51efeb62978b1f22
->>>>>>> 09ffc7ac8d39515288470d5523799c5ef8c7fc6e
+
         }
 
-        public void YellowindIcatorColorToGreenIndicatorColor()
-        {
-            LinearGradientBrush GreenBrush = (LinearGradientBrush)Resources["GreenLinearBrush"];
-            LinearGradientBrush YellowBrush = (LinearGradientBrush)Resources["YellowLinearBrush"];
-            RectangelGrid2.Fill = YellowBrush;
+        //public void YellowindIcatorColorToGreenIndicatorColor()
+        //{
+        //    LinearGradientBrush GreenBrush = (LinearGradientBrush)Resources["GreenLinearBrush"];
+        //    LinearGradientBrush YellowBrush = (LinearGradientBrush)Resources["YellowLinearBrush"];
+        //    RectangelGrid2.Fill = YellowBrush;
 
-            DispatcherTimer ColorTimer2 = new DispatcherTimer();
-            ColorTimer2.Interval = TimeSpan.FromMinutes(45);
-            ColorTimer2.Tick += (Zender, Args) =>
-            {
-                RectangelGrid2.Fill = GreenBrush;
-                ColorTimer2.Stop();
-            };
-            ColorTimer2.Start();
-        }
-
-        public void Image_Loaded(object sender, RoutedEventArgs e)
-        {
-            Image img = sender as Image;
-            BitmapImage bitmapImage = new BitmapImage();
-            bitmapImage.UriSource = new Uri(img.BaseUri, "ms-appx:///Images/test_logo.png");
-            //Image img = new Image();
-            //img.Source = new BitmapImage(new Uri("ms-appx:///Images/test_logo.png"));
-        }
+        //    DispatcherTimer ColorTimer2 = new DispatcherTimer();
+        //    ColorTimer2.Interval = TimeSpan.FromMinutes(45);
+        //    ColorTimer2.Tick += (Zender, Args) =>
+        //    {
+        //        RectangelGrid2.Fill = GreenBrush;
+        //        ColorTimer2.Stop();
+        //    };
+        //    ColorTimer2.Start();
+        //}
 
         private void btn3_Click(object sender, RoutedEventArgs e)
         {
             attributes.Add(new Attributes { RoomID = 1, RoomName = "Rum 1", Projector = true, Wboard = false, Tv = false, Wifi = true, Seats = 5, Note = "Lorem ipsum dolor sit amet, co" });
         }
 
-        private void btn4_Click(object sender, RoutedEventArgs e)
-        {
-            //if (Grid2x3.Visibility == Visibility.Collapsed)
-            //{
-            //    Grid2x3.Visibility = Visibility.Visible;
-            //    Grid2x2.Visibility = Visibility.Collapsed;
-            //    Grid3x2.Visibility = Visibility.Collapsed;
-            //    Grid2x2.Visibility = Visibility.Collapsed;              
-            //    Grid3x2.Visibility = Visibility.Collapsed;
-            //    Grid4x2.Visibility = Visibility.Collapsed;
-            //}
-            //else
-            //{
-            //    Grid2x3.Visibility = Visibility.Collapsed;
-            //}
-        }
-
-        private void btn5_Click(object sender, RoutedEventArgs e)
-        {
-            //if (Grid2x2.Visibility == Visibility.Collapsed)
-            //{
-            //    Grid2x2.Visibility = Visibility.Visible;
-            //    Grid2x3.Visibility = Visibility.Collapsed;
-            //    Grid3x2.Visibility = Visibility.Collapsed;
-            //    Grid3x2.Visibility = Visibility.Collapsed;
-            //    Grid4x2.Visibility = Visibility.Collapsed;
-            //}
-            //else
-            //{
-            //    Grid2x2.Visibility = Visibility.Collapsed;
-            //}
-        }
-
-        private async void imgbtn_Click(object sender, RoutedEventArgs e)
-        {
-            var picker = new Windows.Storage.Pickers.FileOpenPicker();
-            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
-            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
-            picker.FileTypeFilter.Add(".jpg");
-            picker.FileTypeFilter.Add(".jpeg");
-            picker.FileTypeFilter.Add(".png");
-
-            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
-            if (file != null)
-            {
-                //Application now has read/write access to the picked file
-                //this.textBlock.Text = "Picked photo: " + file.Path;
-                //Imgbox1.Source = new ImageSource(file.Path);
-                Logo.Source = new BitmapImage(new Uri(file.Path));
-            }
-            else
-            {
-                //
-            }
-        }
-
-        //public void SwitchGrids()
-        //{
-
-        //    for (var i = 0; i < attributes.Count; i++)
-        //    {
-        //        var count = i;
-        //    }
-        //    switch (attributes.Count)
-        //    {
-        //        case 3:
-        //            {
-        //                break;
-        //            }
-
-        //        case 4:
-        //            {
-        //                break;
-        //            }
-
-        //        case 5:
-        //            {
-        //                break;
-        //            }
-
-        //        case 6:
-        //            {
-        //                break;
-        //            }
-
-        //        case 7:
-        //            {
-        //                break;
-        //            }
-
-        //        case 8:
-        //            {
-        //                break;
-        //            }
-        //    }
-        //}
-
+    
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
 
