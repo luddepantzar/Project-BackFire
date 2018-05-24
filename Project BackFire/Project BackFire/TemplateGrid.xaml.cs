d24ebc24aa0d6156a67e17b5666fe6b54c904390
lubbejareb <ludwigjareb@gmail.com>
2018-05-24 13:19:59 +0200

    push

--- Project BackFire/Project BackFire/TemplateGrid.xaml.cs
+++ Project BackFire/Project BackFire/TemplateGrid.xaml.cs
@@ -88,49 +88,30 @@ namespace Project_BackFire
 
         public async void RedIndicatorColorToYellowIndicatorColor()
         {
-<<<<<<< HEAD
-            StatusColor.Fill = RedBrush;
-=======
->>>>>>> f07dd5734a33f067c6ded6d8ca2ea2b918fc391e
             DispatcherTimer ColorTimer = new DispatcherTimer();
             ColorTimer.Interval = TimeSpan.FromSeconds(7);
             ColorTimer.Tick += async (Sender, args) =>
             {
-<<<<<<< HEAD
-                StatusColor.Fade(duration: 1000, delay: 2000, value: 0).Start();         
-=======
                 await StatusColor.Fade(duration: 1000, delay: 0, value: 0).StartAsync();
                 StatusColor.Fill = RedBrush;
                 await StatusColor.Fade(duration: 1200, delay: 0, value: 1).StartAsync();
 
->>>>>>> f07dd5734a33f067c6ded6d8ca2ea2b918fc391e
                 YellowindIcatorColorToGreenIndicatorColor();
-                StatusColor.Fade(duration: 1000, delay: 0, value: 1).Start();
                 ColorTimer.Stop();
             };
             ColorTimer.Start();
         }
 
-        public  void YellowindIcatorColorToGreenIndicatorColor()
+        public void YellowindIcatorColorToGreenIndicatorColor()
         {
-<<<<<<< HEAD
-            //await StatusColor.Fade(duration: 1000, delay: 1000, value: 0).StartAsync();
-            StatusColor.Fill = YellowBrush;
-=======
->>>>>>> f07dd5734a33f067c6ded6d8ca2ea2b918fc391e
             DispatcherTimer ColorTimer2 = new DispatcherTimer();
             ColorTimer2.Interval = TimeSpan.FromSeconds(7);
             ColorTimer2.Tick += async (Zender, Args) =>
             {
-<<<<<<< HEAD
-                //StatusColor.Fade(duration: 1000, delay: 0, value: 1).Start();
-                StatusColor.Fill = GreenBrush;
-=======
                 await StatusColor.Fade(duration: 1000, delay: 0, value: 0).StartAsync();
                 StatusColor.Fill = YellowBrush;
                 await StatusColor.Fade(duration: 1200, delay: 0, value: 1).StartAsync();
                 red2green();
->>>>>>> f07dd5734a33f067c6ded6d8ca2ea2b918fc391e
                 ColorTimer2.Stop();
             };
             ColorTimer2.Start();
